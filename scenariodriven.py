49feedfd0a404cefd5d0a5f29df84ed950c9dd5b
tudden <Trevor.Udden@gmail.com>
2020-03-30 10:53:26 -0600

    Update scenariodriven.py

--- scenariodriven.py
+++ scenariodriven.py
@@ -144,80 +144,154 @@ class ScenarioDrivenModel:
 ONEDAY = timedelta(1)
 
 def main():
-	model = ScenarioDrivenModel('scenario1.json')
+	 # model = ScenarioDrivenModel('DenverQuarterReducedAge.json')
+    model = ScenarioDrivenModel('DenverQuarterPopulationAdjstments.json')
 
-	model.run()
-	model.gather_sums()
+    # model = ScenarioDrivenModel('DenverQuarter.json')
 
-	u_susc = model.susceptible.domain
-	u_incu = model.incubating.domain
-	u_infe = model.infectious.domain
-	u_isol = model.sum_isolated
-	u_h_no = model.sum_noncrit
-	u_h_ic = model.sum_icu
-	u_h_ve = model.sum_icu_vent
-	u_reco = model.sum_recovered
-	u_dead = model.sum_deceased
+    # model = ScenarioDrivenModel('DenverHalf.json')
+    #  model = ScenarioDrivenModel('DenverFull.json')
+    # model = ScenarioDrivenModel('DenverFull_Rolling.json')
 
-	startdate = model.scenario.initial_date
-	time_domain = [startdate]
-	cursor = startdate
-	for _ in range(0, model.scenario.maxdays):
-		cursor += ONEDAY
-		time_domain.append(cursor)
+    # model = ScenarioDrivenModel('5County.json')
+    # model = ScenarioDrivenModel('7County.json')
+    # model = ScenarioDrivenModel('colorado.json')
 
-#	time_domain = np.linspace(0, model.total_days, model.total_days + 1)
-	hospitalized = []
-	for itr in range(0, len(u_h_no)):
-		print(f"Hospit: {u_h_no[itr] + u_h_ic[itr] + u_h_ve[itr]}")
-		hospitalized.append(u_h_no[itr] + u_h_ic[itr] + u_h_ve[itr])
+    model.run()
+    model.gather_sums()
+
+    u_susc = model.susceptible.domain
+    u_incu = model.incubating.domain
+    u_infe = model.infectious.domain
+    u_isol = model.sum_isolated
+    u_h_no = model.sum_noncrit
+    u_h_ic = model.sum_icu
+    u_h_ve = model.sum_icu_vent
+    u_reco = model.sum_recovered
+    u_dead = model.sum_deceased
+
+    time_domain = np.linspace(0, model.total_days, model.total_days + 1)
+    hospitalized = []
+    for itr in range(0, len(u_h_no)):
+        hospitalized.append(u_h_no[itr] + u_h_ic[itr] + u_h_ve[itr])
+    #print(model.scenario.
+    print(model.r0)
+
+    fig = plt.figure(facecolor='w')
+    # ax = fig.add_subplot(111, axis_bgcolor='#dddddd', axisbelow=True)
+    ax = fig.add_subplot(111, axisbelow=True)
+
+    #------------
+    #basic SIER
+    #------------
+
+    #SEIR
+    # ax.plot(time_domain, u_susc, color=TABLEAU_BLUE, alpha=.5, lw=2, label='Susceptible', linestyle='-')
+    # ax.plot(time_domain, u_incu, color=TABLEAU_ORANGE, alpha=.75, lw=2, label='Exposed', linestyle='-')
+    # ax.plot(time_domain, u_infe, color=TABLEAU_RED, alpha=0.5, lw=2, label='Infected', linestyle='-')
+    # # ax.plot(time_domain, u_isol, color=TAB_COLORS[8], alpha=.5, lw=2, label='Home Isolated', linestyle='-')
+    # ax.plot(time_domain, u_reco, color=TABLEAU_GREEN, alpha=.5, lw=2, label='Recovered', linestyle='--')
+    # # ax.plot(time_domain, u_dead, color=(0, 0, 0), alpha=.5, lw=2, label='Dead', linestyle=':')
+
+    #------------
+    #hosp cap
+    #------------
+    ax.plot(time_domain, u_h_no, color=TABLEAU_GREEN, alpha=1, lw=1.5, label='Demand - Non-Critical', linestyle='-')
+    ax.plot(time_domain, u_h_ic, color=TABLEAU_ORANGE, alpha=1, lw=1.5, label='Demand - ICU (w/o Ventilator)', linestyle='--')
+    ax.plot(time_domain, u_h_ve, color=TABLEAU_RED, alpha=1, lw=1.5, label='Demand - ICU (w/ Ventilator)', linestyle='--')
+    ax.plot(time_domain, hospitalized, color=TABLEAU_BLUE, alpha=1, lw=1.5, label='Demand - Total Hospitalized', linestyle='-')
+    ax.plot(time_domain, u_h_ic+u_h_ve, color=TABLEAU_PURPLE, alpha=1, lw=1.5, label='Demand - Total ICU', linestyle='-')
+
+    # ------------
+    # Capacity Lines - Denver County - Empty
+    # ------------
+
+    # normal
+    # ax.plot(time_domain, [695] * (model.total_days + 1), color=(0, 0, 0), alpha=.8, lw=1.5, label='Supply - 5C ICU Beds', linestyle='-')
+    # ax.plot(time_domain, [5907] * (model.total_days + 1), color=(0, 0, 0), alpha=1, lw=1.5, label='Supply - 5C Total Beds', linestyle='-')
+
+    # surge 1.5
+    # ax.plot(time_domain, [1043] * (model.total_days + 1), color=(0, 0, 0), alpha=.5, lw=1.5, label='Supply - 1.5x 5C ICU Beds', linestyle='--')
+    # ax.plot(time_domain, [8861] * (model.total_days + 1), color=(0, 0, 0), alpha=.5, lw=1.5, label='Supply - 1.5x 5C Total Beds', linestyle='--')
+
+    #------------
+    # Capacity Lines - 5 County - Empty
+    #------------
+
+    # normal
+    # ax.plot(time_domain, [255] * (model.total_days + 1), color=(0, 0, 0), alpha=.8, lw=1.5, label='Supply - 2x County ICU Beds', linestyle='-')
+    # ax.plot(time_domain, [1000] * (model.total_days + 1), color=(0, 0, 0), alpha=1, lw=1.5, label='Supply - 70% County Total Beds', linestyle='-')
+
+    #surge 1.5
+    # ax.plot(time_domain, [1043] * (model.total_days + 1), color=(0, 0, 0), alpha=.5, lw=1.5, label='Supply - 1.5x 5C ICU Beds', linestyle='--')
+    # ax.plot(time_domain, [4135] * (model.total_days + 1), color=(0, 0, 0), alpha=.5, lw=1.5, label='Supply - 70% 5C Total Beds', linestyle='--')
+
+    # ------------
+    # Capacity Lines - 5 County - 50%
+    # ------------
+
+    # ax.plot(time_domain, [348] * (model.total_days + 1), color=(0, 0, 1), alpha=1, lw=1, label='Supply - 5County ICU', linestyle='--')
+    # ax.plot(time_domain, [2954] * (model.total_days + 1), color=(1, 0, 0), alpha=1, lw=1, label='5County Tot', linestyle='-')
+    #
+    # ax.plot(time_domain, [521] * (model.total_days + 1), color=(0, 0, 1), alpha=1, lw=1, label='1.5x 5County ICU', linestyle='--')
+    # ax.plot(time_domain, [4430] * (model.total_days + 1), color=(1, 0, 0), alpha=1, lw=1, label='1.5x 5County Tot', linestyle='-')
+    #
+    # ------------
+    # Capacity Lines - DH  - Specific
+    # # ------------
+    ax.plot(time_domain, [86] * (model.total_days + 1), color=(0, 0, 0), alpha=.7, lw=1, label='Supply - DH ICU Beds', linestyle='--')
+    ax.plot(time_domain, [129] * (model.total_days + 1), color=(0, 0, 0), alpha=1, lw=1, label='Supply - DH Total Beds', linestyle='-')
 
 
-	fig = plt.figure(facecolor='w')
-	# ax = fig.add_subplot(111, axis_bgcolor='#dddddd', axisbelow=True)
-	ax = fig.add_subplot(111, axisbelow=True)
-#	ax.plot(time_domain, u_susc, color=(0, 0, 1), alpha=.5, lw=2, label='Susceptible', linestyle='-')
-#	ax.plot(time_domain, u_incu, color=TABLEAU_ORANGE, alpha=0.1, lw=2, label='Exposed', linestyle='-')
-#	ax.plot(time_domain, u_infe, color=TABLEAU_RED, alpha=0.5, lw=2, label='Infected', linestyle='-')
-#	ax.plot(time_domain, u_isol, color=TAB_COLORS[8], alpha=.5, lw=2, label='Home Iso', linestyle='-')
-	ax.plot(time_domain, u_h_no, color=TABLEAU_BLUE, alpha=1, lw=2, label='Noncrit', linestyle='--')
-	ax.plot(time_domain, u_h_ic, color=TABLEAU_GREEN, alpha=1, lw=2, label='ICU', linestyle='--')
-	ax.plot(time_domain, u_h_ve, color=TABLEAU_RED, alpha=1, lw=2, label='ICU + Ventilator', linestyle='--')
-	ax.plot(time_domain, hospitalized, color=(1, 0, 0), alpha=.25, lw=2, label='Total Hospitalized', linestyle='-')
-#	ax.plot(time_domain, u_reco, color=(0, .5, 0), alpha=.5, lw=2, label='Recovered', linestyle='--')
-#	ax.plot(time_domain, u_dead, color=(0, 0, 0), alpha=.5, lw=2, label='Dead', linestyle=':')
+    #make pretty
 
-	ax.plot(time_domain, [511] * (model.total_days + 1), color=(0, 0, 1), alpha=1, lw=1, label='511 Beds', linestyle='-')
-	ax.plot(time_domain, [77] * (model.total_days + 1), color=(1, 0, 0), alpha=1, lw=1, label='77 ICU units', linestyle='-')
-	plt.axvline(x=datetime.today(), alpha=.5, lw=2, label='Today')
+    # set the style of the axes and the text color
+    plt.rcParams['axes.edgecolor'] = '#333F4B'
+    plt.rcParams['axes.linewidth'] = 0.8
+    plt.rcParams['xtick.color'] = '#333F4B'
+    plt.rcParams['ytick.color'] = '#333F4B'
+    plt.rcParams['text.color'] = '#333F4B'
 
-	ax.set_xlabel('Days')
-	ax.set_ylabel('Number')
+    # set axis
+    ax.tick_params(axis='both', which='major', labelsize=12)
 
-	chart_title = model.modelname
-	plt.title(chart_title, fontsize=14)
-	# ax.set_ylim(0,1.2)
-	ax.yaxis.set_tick_params(length=4)
-	ax.xaxis.set_tick_params(length=4)
-	# ax.grid(b=True, which='minor', c='w', lw=1, ls='--')
-	ax.grid()
-	legend = ax.legend()
-	legend.get_frame().set_alpha(0.5)
-	for spine in ('top', 'right', 'bottom', 'left'):
-		ax.spines[spine].set_visible(False)
+    # set labels
+    ax.set_xlabel('Days')
+    ax.set_ylabel('Number')
 
-	outfilename = "_".join(chart_title.replace("|", " ").replace(":", " ").replace(".", " ").split())
 
-	# Write a CSV to this directory
-	with open(f"{outfilename}.csv", 'w') as outfile:
-		for itr in range(0, len(u_susc)):
-			outfile.write(f"{u_susc[itr]:.6f}, {u_incu[itr]:.6f}, {u_infe[itr]:.6f}, {u_isol[itr]:.6f}"
-			              f", {u_h_no[itr]:.6f}, {u_h_ic[itr]:.6f}, {u_h_ve[itr]:.6f}, {u_reco[itr]:.6f}"
-			              f", {u_dead[itr]:.6f}, {hospitalized[itr]:.6f}\n")
+    chart_title = model.modelname
+    plt.title(chart_title, fontsize=14)
 
-	plt.savefig(f"{outfilename}.png", bbox_inches="tight")
-	plt.show()
+    ax.yaxis.set_tick_params(length=4)
+    ax.xaxis.set_tick_params(length=4)
 
+    # ax.grid(b=True, which='minor', c='w', lw=1, ls='--')
+    ax.grid()
+    legend = ax.legend()
+    legend.get_frame().set_alpha(0.5)
+
+    for spine in ('top', 'right', 'bottom', 'left'):
+        ax.spines[spine].set_visible(False)
+
+    outfilename = "_".join(chart_title.replace("|", " ").replace(":", " ").replace(".", " ").split())
+
+    # Write a CSV to this directory
+    with open(f"{outfilename}.csv", 'w') as outfile:
+        for itr in range(0, len(u_susc)):
+            outfile.write(f"{u_susc[itr]:.6f}, "
+                          f"{u_incu[itr]:.6f}, "
+                          f"{u_infe[itr]:.6f}, "
+                          f"{u_isol[itr]:.6f}, "
+                          f"{u_h_no[itr]:.6f}, "
+                          f"{u_h_ic[itr]:.6f}, "
+                          f"{u_h_ve[itr]:.6f}, "
+                          f"{u_reco[itr]:.6f}, "
+                          f"{u_dead[itr]:.6f}, "
+                          f"{hospitalized[itr]:.6f}\n")
+
+    plt.savefig(f"{outfilename}.png", bbox_inches="tight")
+    plt.show()
 
 if __name__ == '__main__':
 	main()
